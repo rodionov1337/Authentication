4fa4ad17a30d74fe9f6c0e175751d74c2a14dc52
Evgenii <zhekarodionov@icloud.com>
2020-05-18 14:48:46 +0300

    Added info

--- src/main/java/Auth.java
+++ src/main/java/Auth.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Auth {
-    DB db = new Database("");
+    DB db = new Database("");// enter path to database
 
     public Auth() throws RocksDBException {
     }
